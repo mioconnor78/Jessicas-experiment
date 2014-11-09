979a31e601ba63467ba88431df54f9f926e04915
Mary <oconnor@zoology.ubc.ca>
2014-11-09 13:36:58 -0800

    wrote code for plot and analysis of total NPP vs invT at week 8

--- week 8 code.R
+++ week 8 code.R
@@ -14,3 +14,25 @@ library(nlme)
 k <- 8.62*10^-5
 data$invT <-  1/((data$average.temp + 273)*k)
 data$invT <- as.numeric(as.character(data$invT))
+
+## Does NPP vary with temperature?  
+## figures
+hist(data$NPP)
+plot(log(data$NPP)~data$invT, cex=1.5, pch='',  axes=FALSE, ylim=c(-3,2), xlim=c(38.5,41), xlab='inv Temperature (C) at week 8', ylab='NPP log(mg/L/hr)') 
+axis(1, at=c(38.5,39, 39.5, 40,40.5, 41), pos=-3, lwd=2, cex.lab=1.5)
+axis(2, at=c(-3,-2,-1,0,1,2), pos=38.5, lwd=2, cex.lab=1.5)
+points(log(data[(data$trophic.level=='P'),]$NPP)~data[(data$trophic.level=='P'),]$invT, pch=19)
+points(log(data[(data$trophic.level=='PZ'),]$NPP)~data[(data$trophic.level=='PZ'),]$invT, pch=22)
+points(log(data[(data$trophic.level=='PZN'),]$NPP)~data[(data$trophic.level=='PZN'),]$invT, pch=2)
+
+## analysis
+modNPP1<-lm(log(data$NPP)~data$invT)
+modNPP2<-lm(log(data$NPP)~data$invT*data$trophic.level)
+anova(modNPP1, modNPP2)
+summary(modNPP2)
+coef(modNPP2)
+
+## add lines to plot
+abline(coef(modNPP2)[1], coef(modNPP2)[2], lty = 1, lwd = 3)
+abline((coef(modNPP2)[1]+coef(modNPP2)[3]), (coef(modNPP2)[2]+coef(modNPP2)[5]), lty = 2, lwd = 3)
+abline((coef(modNPP2)[1]+coef(modNPP2)[4]), (coef(modNPP2)[2]+coef(modNPP2)[6]), lty = 3, lwd = 3)
