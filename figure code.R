ef6637614a35039e07624b46beed558ce4364178
Mary <oconnor@zoology.ubc.ca>
2015-08-26 07:43:06 -0700

    added scatter3ds for NPP and PP biomass

--- figure code.R
+++ figure code.R
@@ -7,4 +7,22 @@ library(rgl)
 
 plot(log(data$calc.NPP)~data$invT, pch = as.numeric(data$trophic.level)+14, col = data$trophic.level)
 
-scatter3d(data$invT, log(data$calc.NPP), dadta$week)
+scatter3d(data$invT, log(data$calc.NPP), data$week, point.col = as.numeric(data$trophic.level), surface = FALSE)
+
+
+scatter3d(data$week, log(data$calc.NPP), data$invT, point.col = as.numeric(data$trophic.level), surface = FALSE)
+
+
+scatter3d(data$week, log(data$PP.biomass), data$invT, point.col = as.numeric(data$trophic.level), surface = TRUE)
+scatter3d(data$invT, log(data$PP.biomass), data$week, point.col = as.numeric(data$trophic.level), surface = FALSE)
+
+#not working for some reason
+scatter3d(dataz$week, log(dataz$zoo.ug.carbon.liter), dataz$invT, point.col = as.numeric(dataz$trophic.level), surface = TRUE)
+
+
+savePlot(file = "JG3dNPP.pdf", type = "jpeg")
+
+
+jpeg(file = "JG3dNPP.jpg", width = 5, height = 5)
+scatter3d(data$week, log(data$calc.NPP), data$invT, point.col = as.numeric(data$trophic.level), surface = FALSE)
+dev.off()
