b1e437b3256dfeb663dca87142a5b09c54512587
Mary <oconnor@zoology.ubc.ca>
2016-03-20 15:36:30 -0700

    fixed minor error in size correction code, and added code for plot to compare Mt and corrected M

--- mass size correction.R
+++ mass size correction.R
@@ -9,6 +9,8 @@
 ### size-corrected biomass analysis
 
 library(plyr)
+library(broom)
+library(dplyr)
 
 ### following Barneche et al 2014
 
@@ -17,7 +19,8 @@ library(plyr)
 ### size corrected biomass
 
 sdata <- read.csv("./zooplankton size for correction no eggs.csv")
-sdata$drywt <- ifelse(sdata$species == 'Daphnia', exp(log(1.468 + 2.829*log(sdata$size))), exp(log(1.821 + 0.654*log(sdata$size))))
+names(sdata) <- c('treatment', 'tank', 'week', 'species', 'stage', 'size')
+sdata$drywt <- ifelse(sdata$species == 'Daphnia', exp(1.468 + 2.829*log(sdata$size)), exp(1.821 + 0.654*log(sdata$size)))
 sdata$C.ug <- sdata$drywt*0.5
 
 #add in notonectids
@@ -25,7 +28,13 @@ sdata$noto.wt.corr <- ifelse(sdata$treatment == 'PZN', 2*((10.8/4)^.75), 0) #add
 mass.data <- ddply(sdata, .(week, tank, treatment), summarise, sum((C.ug^0.75))) # estimating a mass corrected biomass for adult zooplankton in all tanks.
 mass.data2 <- ddply(sdata, .(week, tank, treatment), summarise, sum((C.ug^0.75)+noto.wt.corr)) # estimating a mass corrected biomass for adult zooplankton incl Noto in all tanks.
 mass.data3 <- ddply(sdata, .(week, tank, treatment), summarise, sum((C.ug))) 
-mass.data$M.corrz <- mass.data2$..1
-mass.data$zpC.uncorr <- mass.data3$..1
+mass.data$M.corrz <- mass.data2$..1 # corrected total zooplankton
+mass.data$zpC.uncorr <- mass.data3$..1  # uncorrected total (no notonectids)
 names(mass.data) <- c('week', 'tank', 'treatment', 'M.corrT', 'M.corrz', 'M.uncorrz')
 
+
+## estimating a ratio of metabolic biomass to total biomass. T is total biomass.
+msizefunc <- function(n, T) n*((T/n)^0.75)
+plot(seq(1,60,1), msizefunc(seq(1,60,1), 12), xlim=c(0,60), ylim = c(0,60), xlab = 'number of individuals', ylab = 'metabolic biomass')
+abline(h = 12)
+abline(v = 12)
